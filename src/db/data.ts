6adac92ba22a17baeabd7f1f661a966c6bdadfbe
Eduardo <eduardo.robettibedin@gmail.com>
2026-05-10 20:44:19 -0300

    Remove unused image assets and update data structure to include new commerce entries; enhance Home screen to display both featured items and nearby commerces.

--- src/db/data.ts
+++ src/db/data.ts
@@ -6,20 +6,67 @@ type Destaque = {
   image: ImageSourcePropType;
   descricao: string;
   marca: string;
-}
-export const destaques: Destaque[]  =[ 
+};
+type Comercio = {
+  id: number;
+  nome: string;
+  image: ImageSourcePropType;
+  descricao: string;
+  telefone: string;
+  instagram: string;
+  email: string;
+};
+
+export const comercios: Comercio[] = [
+  {
+    id: 1,
+    nome: "Tree",
+    image: require("../assets/images/comercio_cogumelo.png"),
+    descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+    telefone: '(48) 99999-9999', 
+    instagram: 'cogumelo@',
+    email: 'cogumelo@gmail.com'
+  },
+  {
+    id: 2,
+    nome: "Nature",
+    image: require("../assets/images/comercio_suco.png"),
+    descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+    telefone: '(48) 99999-9934', 
+    instagram: 'suco@',
+    email: 'suco@gmail.com'
+  },
+  {
+    id: 3,
+    nome: "Geleia",
+    image: require("../assets/images/comercio_geleia.png"),
+    descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+    telefone: '(48) 99999-9934', 
+    instagram: 'geleia@',
+    email: 'geleia@gmail.com'
+  },
+];
+
+export const destaques: Destaque[] = [
+  {
+    id: 1,
+    preco: "33,00",
+    image: require("../assets/images/amendoin.png"),
+    descricao: "Amendoin",
+    marca: "Tree",
+  },
   {
     id: 2,
     preco: "7,90",
-    image: require("../assets/suco.png"),
+    image: require("../assets/images/suco.png"),
     descricao: "Suco de uva 1L",
     marca: "Sucos Zottenelli",
   },
   {
     id: 3,
     preco: "12,50",
-    image: require("../assets/cogumelo.png"),
+    image: require("../assets/images/cogumelo.png"),
     descricao: "Cogumelo fresco",
     marca: "Verde Alegre",
-  }
-]
+  },
+];
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,13 @@
-import { View, Text, Image, FlatList } from "react-native";
+import { View, Text, Image, FlatList, ScrollView } from "react-native";
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { destaques } from "../../db/data";
+import { destaques, comercios } from "../../db/data";
 
 export default function Home() {
   const edges = useSafeAreaInsets();
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
+    <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
       <View
         style={{
           width: "100%",
@@ -27,46 +27,91 @@ export default function Home() {
         >
           <FontAwesome name="bars" size={24} color="white" />
           <Image
-            source={require("../../assets/logo.png")}
+            source={require("../../assets/images/oeste_organicos.png")}
             style={{ width: 100, height: 30 }}
           />
           <FontAwesome name="search" size={24} color="white" />
         </View>
       </View>
       <View style={{ flex: 1, paddingHorizontal: 20 }}>
-        <View style={{ width: "100%", height: 200 }}>
+        <View style={{ width: "100%", height: 170 }}>
           <Image
-            source={require("../../assets/banner.png")}
+            source={require("../../assets/images/banner.png")}
             style={{ width: "100%", height: "100%" }}
             resizeMode="contain"
           />
         </View>
-        <FlatList
-          data={destaques}
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(item) => item.id.toString()}
-          horizontal
-          contentContainerStyle={{ gap: 10, height: 200 }}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: "#026534",
-                borderRadius: 10,
-                padding: 10,
-              }}
-            >
-              <Image source={item.image} style={{ width: 120, height: 100, borderRadius: 10 }}  />
-              <View style={{marginTop: 10, gap: 3}}>
-              <Text style={{ fontSize: 16, fontWeight: "bold", color: "#CAFF62" }}>R$: {item.preco}</Text>
-                <Text style={{ fontSize: 10, fontWeight: "bold", color: "white" }}>{item.descricao}</Text>
-                
-                <Text style={{ fontSize: 14, fontWeight: "bold", color: "#CAFF62" }}>{item.marca}</Text>
+        <View style={{ gap: 10 }}>
+          <Text style={{ marginBottom: 5, fontSize: 14, color: "#026534" }}>
+            Comércios Próximos
+          </Text>
+          <FlatList
+            data={comercios}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal
+            contentContainerStyle={{ gap: 10 }}
+            renderItem={({ item }) => (
+              <Image
+                source={item.image}
+                style={{ width: 100, height: 100, borderRadius: 20 }}
+              />
+            )}
+          />
+          <Text style={{ marginBottom: 5, fontSize: 14, color: "#026534" }}>
+            Destaques
+          </Text>
+          <FlatList
+            data={destaques}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal
+            contentContainerStyle={{ gap: 10 }}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                  backgroundColor: "#026534",
+                  borderRadius: 5,
+                  padding: 10,
+                  width: 120,
+                }}
+              >
+                <Image
+                  source={item.image}
+                  style={{ width: 100, height: 90, borderRadius: 5 }}
+                />
+                <View style={{ marginTop: 10, gap: 3 }}>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontWeight: "bold",
+                      color: "#CAFF62",
+                    }}
+                  >
+                    R$: {item.preco}
+                  </Text>
+                  <Text
+                    style={{ fontSize: 10, fontWeight: "bold", color: "white" }}
+                  >
+                    {item.descricao}
+                  </Text>
+
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "bold",
+                      color: "#CAFF62",
+                    }}
+                    numberOfLines={1}
+                  >
+                    {item.marca}
+                  </Text>
+                </View>
               </View>
-            </View>
-          )}
-        />
+            )}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
